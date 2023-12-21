3e791be0d7dad0bc8cc826128a1876d5d198f7a9
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2023-12-21 17:38:17 +0700

    Update example for JavaScript in JSX with Curly Braces

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -15,8 +15,37 @@ function App() {
         <li>Rehearse a movie scene</li>
         <li>Improve the spectrum technology</li>
       </ul>
+      <Avatar />
+      <TodoList/>
     </>
   )
 }
 
 export default App
+
+export const  Avatar = () =>{
+  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+  const description = 'Gregorio Y. Zara';
+  return (
+    <img
+      className="avatar"
+      src={avatar}
+      alt={description}
+    />
+  );
+}
+
+const today = new Date();
+
+const formatDate = (date) => {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
+
+export const TodoList = () => {
+  return (
+    <h1>To Do List for {formatDate(today)}</h1>
+  );
+}
