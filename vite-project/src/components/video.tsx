2108b04b383618edb01462cafe54408dd5701314
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2023-12-26 10:52:15 +0700

    Add [isPlaying] for checking if else conditions

--- vite-project/src/components/video.tsx
+++ vite-project/src/components/video.tsx
@@ -6,19 +6,23 @@ const VideoPlayer = ({ src, isPlaying }) => {
 
   useEffect(() => {
     if (isPlaying) {
+      console.log('Calling video.play()');
       ref.current.play();
     } else {
+      console.log('Calling video.pause()');
       ref.current.pause();
     }
-  });
+  }, [isPlaying]);
 
   return <video ref={ref} src={src} loop playsInline />
 }
 
 const Video = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [text, setText] = useState('');
   return (
     <div>
+      <input value={text} onChange={e => setText(e.target.value)} />
       <button onClick={() => setIsPlaying(!isPlaying)}>
         {isPlaying ? 'Pause' : 'Play'}
       </button>
@@ -27,7 +31,7 @@ const Video = () => {
         src='https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4'
       />
     </div>
-  )
+  );
 }
 
 export default Video
