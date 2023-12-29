3917758a747b1d626d5f3c62202cc7da15b6f854
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2023-12-29 14:43:17 +0700

    Use the aside tag instead of the main tag in the sidebar

--- practice-react/src/components/sidebar/index.tsx
+++ practice-react/src/components/sidebar/index.tsx
@@ -12,21 +12,21 @@ import {
 const Sidebar = () => {
   return (
     <div className='container-sidebar'>
-      <main className='main-sidebar hide-sidebar' id='mainsidebar'>
-      <Profile
+      <aside className='main-sidebar hide-sidebar' id='mainsidebar'>
+        <Profile
           title='CRUD OPERATIONS'
           avatar= {avatar}
           alt='avatar'
           userName='Karthi Madesh'
           description='Admin'
         />
-        <aside className='sub-sidebar-content'>
+        <div className='sub-sidebar-content'>
           <Menu />
           <div className='navigation'>
             <Button className='btn-logout' ariaLabel='logout' name='Logout' icon={logout} />
           </div>
-        </aside>
-      </main>
+        </div>
+      </aside>
     </div>
   )
 }
