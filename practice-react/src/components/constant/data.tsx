6c62541c38c0c56c830f93889f6ae5c8c3e34f82
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2023-12-29 14:16:01 +0700

    Update code style for data.tsx file

--- practice-react/src/components/constant/data.tsx
+++ practice-react/src/components/constant/data.tsx
@@ -9,25 +9,25 @@ import {
 
 export const MENU_LIST = [
   {
-    icon:home,
-    name:'Home',
-    alt:'icon home',
-    ariaLabel:'Go to home page',
-    href:'javascript:void(0);'
+    icon: home,
+    name: 'Home',
+    alt: 'icon home',
+    ariaLabel: 'Go to home page',
+    href: 'javascript:void(0);'
   },
   {
-    icon:course,
-    name:'Course',
-    alt:'icon Course',
-    ariaLabel:'Go to course page',
-    href:'javascript:void(0);'
+    icon: course,
+    name: 'Course',
+    alt: 'icon Course',
+    ariaLabel: 'Go to course page',
+    href: 'javascript:void(0);'
   },
   {
-    icon:graduation,
-    name:'Students',
-    alt:'icon Students',
-    ariaLabel:'Go to student page',
-    href:'javascript:void(0);'
+    icon: graduation,
+    name: 'Students',
+    alt: 'icon Students',
+    ariaLabel: 'Go to student page',
+    href: 'javascript:void(0);'
   },
   {
     icon:payment,
@@ -37,17 +37,17 @@ export const MENU_LIST = [
     href:'javascript:void(0);'
   },
   {
-    icon:report,
-    name:'report',
-    alt:'icon report',
-    ariaLabel:'Go to report page',
-    href:'javascript:void(0);'
+    icon: report,
+    name: 'report',
+    alt: 'icon report',
+    ariaLabel: 'Go to report page',
+    href: 'javascript:void(0);'
   },
   {
-    icon:setting,
-    name:'Settings',
-    alt:'icon Settings',
-    ariaLabel:'Go to Setting page',
-    href:'javascript:void(0);'
+    icon: setting,
+    name: 'Settings',
+    alt: 'icon Settings',
+    ariaLabel: 'Go to Setting page',
+    href: 'javascript:void(0);'
   }
 ]
