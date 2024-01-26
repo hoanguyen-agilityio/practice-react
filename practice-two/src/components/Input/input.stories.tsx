2258b8ec19d987bbb4878f2982fb9c2c4c060046
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-01-26 16:41:15 +0700

    Create a storybook for the component's password input

--- practice-two/src/components/Input/input.stories.tsx
+++ practice-two/src/components/Input/input.stories.tsx
@@ -14,11 +14,23 @@ export default {
 
 type Story = StoryObj<typeof Input>;
 
+// Default input
 export const DefaultInput: Story = {
   args: {
     type: 'text',
     name: 'input',
     ariaLabel: 'Input default',
+    customClass: 'w-445px'
+  },
+};
+
+// Password input
+export const PasswordInput: Story = {
+  args: {
+    type: 'password',
+    name: 'password',
+    ariaLabel: 'Input password',
+    placeholder: '••••••••••••••••••••',
     customClass: 'w-445px'
   },
 };
